8c82063299292f8ff07ad1f942d407bc0b5c4341
simonatingleffkardel <simonakardel@gmail.com>
2023-03-02 10:03:22 +0100

    file path changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("public")); 
+app.use(express.static(__dirname + "/public/"));
 const port = 3000;
 
 app.get('/', async (req, res) => {
